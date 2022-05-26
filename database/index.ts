99aca90e5e4ef17383701c2056b728d6f361f30e
sachin-saara <shukla.s@saara.io>
2022-05-26 13:08:15 +0530

    Update Schema Version

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,6 @@
 export * from 'database/static/landing';
 export * from 'database/local/indexDB';
+export * from 'database/local/schema';
 export * from 'database/models';
 export * from 'database/modules/user';
 export * from 'database/modules/contact';
